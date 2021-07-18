ecce841b69d65597f4b7a7382249f406af4e5ab4
asrani <asrani.27@gmail.com>
2021-07-18 23:54:15 +0800

    f

--- resources/views/pegawai/presensi/barcode/presensi.blade.php
+++ resources/views/pegawai/presensi/barcode/presensi.blade.php
@@ -85,7 +85,7 @@ crossorigin=""/>
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       if (cameras.length > 0) {
-        scanner.start(cameras[2]);
+        scanner.start(cameras[1]);
       } else {
         console.error('No cameras found.');
       }
