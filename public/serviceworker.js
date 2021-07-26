074c65216dca7520ad445d6f790547dc21d0164e
asrani <asrani.27@gmail.com>
2021-07-26 11:51:06 +0800

    f

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,8 +1,8 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
     //'/offline',
-    '/css/app.css',
-    '/js/app.js',
+    //'/css/app.css',
+    //'/js/app.js',
     '/images/icons/icon-72x72.png',
     '/images/icons/icon-96x96.png',
     '/images/icons/icon-128x128.png',
@@ -24,19 +24,19 @@ self.addEventListener("install", event => {
     )
 });
 
-// Clear cache on activate
-// self.addEventListener('activate', event => {
-//     event.waitUntil(
-//         caches.keys().then(cacheNames => {
-//             return Promise.all(
-//                 cacheNames
-//                     .filter(cacheName => (cacheName.startsWith("pwa-")))
-//                     .filter(cacheName => (cacheName !== staticCacheName))
-//                     .map(cacheName => caches.delete(cacheName))
-//             );
-//         })
-//     );
-// });
+//Clear cache on activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith("pwa-")))
+                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
 
 // Serve from Cache
 // self.addEventListener("fetch", event => {
