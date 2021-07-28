cc6ad6065e4e2b8758cc3c08fe4559196c4dd205
asrani <asrani.27@gmail.com>
2021-07-28 09:45:22 +0800

    f

--- resources/views/pegawai/home.blade.php
+++ resources/views/pegawai/home.blade.php
@@ -161,6 +161,9 @@ crossorigin=""/>
             </div>
         </div> --}}
 OS : {{$os}}
+{{new \Jenssegers\Agent\Agent())->isMobile()}}
+{{new \Jenssegers\Agent\Agent())->isAndroid()}}
+{{new \Jenssegers\Agent\Agent())->isSafari()}}
 @endsection 
 
 @push('js')
