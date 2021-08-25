1446fc7fcccd156dad57d7806a049abe4a32d7e7
asrani <asrani.27@gmail.com>
2021-08-25 08:20:38 +0800

    f

--- resources/views/pegawai/home.blade.php
+++ resources/views/pegawai/home.blade.php
@@ -187,8 +187,8 @@ crossorigin=""/>
 @endsection 
 
 @push('js')
-@include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
-{{Session::forget('sweet_alert');}}
+{{-- @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
+{{Session::forget('sweet_alert');}} --}}
 {{-- @include('sweetalert::alert') --}}
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
 integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
