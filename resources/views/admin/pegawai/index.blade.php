23371acf5b77461477674dfdffcce24afe9c8c85
asrani <asrani.27@gmail.com>
2021-12-21 10:36:38 +0800

    hapus tombol urut pegawai

--- resources/views/admin/pegawai/index.blade.php
+++ resources/views/admin/pegawai/index.blade.php
@@ -1,92 +1,101 @@
 @extends('layouts.app')
 
 @push('css')
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 @endpush
 @section('title')
-    ADMIN
+ADMIN
 @endsection
 @section('content')
-<br/>
+<br />
 <div class="row">
     <div class="col-12">
-        <a href="/admin/pegawai/sync" class="btn btn-sm bg-gradient-purple"><i class="fas fa-sync"></i> Sinkronisasi Pegawai</a>
-        <a href="/admin/pegawai/createuser" class="btn btn-sm bg-gradient-green"><i class="fas fa-key"></i> Buat Akun Login</a>
-        <a href="/admin/pegawai/sortir" class="btn btn-sm bg-gradient-info"><i class="fas fa-users"></i> Urutkan Pegawai</a>
-        <br/><br/>
+        <a href="/admin/pegawai/sync" class="btn btn-sm bg-gradient-purple"><i class="fas fa-sync"></i> Sinkronisasi
+            Pegawai</a>
+        <a href="/admin/pegawai/createuser" class="btn btn-sm bg-gradient-green"><i class="fas fa-key"></i> Buat Akun
+            Login</a>
+        {{-- <a href="/admin/pegawai/sortir" class="btn btn-sm bg-gradient-info"><i class="fas fa-users"></i> Urutkan
+            Pegawai</a> --}}
+        <br /><br />
         <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Data Pegawai</h3>
-            <div class="card-tools">
-                <form method="get" action="/admin/pegawai/search">
-                <div class="input-group input-group-sm" style="width: 250px;">
-                    <input type="text" name="search" class="form-control float-right" value="{{old('search')}}" placeholder="Nama / NIP">
+            <div class="card-header">
+                <h3 class="card-title">Data Pegawai</h3>
+                <div class="card-tools">
+                    <form method="get" action="/admin/pegawai/search">
+                        <div class="input-group input-group-sm" style="width: 250px;">
+                            <input type="text" name="search" class="form-control float-right" value="{{old('search')}}"
+                                placeholder="Nama / NIP">
 
-                    <div class="input-group-append">
-                    <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                    </div>
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                </form>
             </div>
-        </div>
-        <!-- /.card-header -->
-        <div class="card-body table-responsive p-0">
-            <table class="table table-hover text-nowrap table-sm">
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>NIP/Nama/Jabatan</th>
-                <th>Pangkat</th>
-                <th>Tgl Lahir</th>
-                <th>Lokasi Presensi</th>
-                <th>Aksi</th>
-                </tr>
-            </thead>
-            @php
-                $no =1;
-            @endphp
-            <tbody>
-            @foreach ($data as $key => $item)
-                    @if ($item->is_aktif == 0)                        
-                    <tr style="font-size:11px; font-family:Arial, Helvetica, sans-serif; background-color:#f2dede">
-                    @else                        
-                    <tr style="font-size:11px; font-family:Arial, Helvetica, sans-serif; ">
-                    @endif
-                    <td>{{$no++}}</td>
-                    <td>{{$item->nama}}<br/>{{$item->nip}}<br/>{{$item->jabatan}}</td>
-                    <td>{{$item->pangkat}}</td>
-                    <td>{{\Carbon\Carbon::parse($item->tanggal_lahir)->format('d-m-Y')}}</td>
-                    <td>
-                        
-                    @if ($item->is_aktif == 0) 
-                    @else
-                        @if ($item->lokasi == null)
-                        <a href="/admin/pegawai/{{$item->id}}/lokasi" class="btn btn-xs bg-gradient-primary"><i class="fas fa-pen"></i></a>
-                        @else
-                            {{$item->lokasi->nama}}
-                            <a href="/admin/pegawai/{{$item->id}}/editlokasi" class="btn btn-xs bg-gradient-success"><i class="fas fa-pen"></i></a>
-                        @endif
-                    @endif
-                    </td>
-                    <td>
-                        @if ($item->is_aktif == 0)     
-                        <strong>STATUS : PENSIUN</strong>
-                        
-                        @else
-                            @if ($item->user == null)
-                            <a href="/admin/pegawai/{{$item->id}}/createuser" class="btn btn-xs bg-gradient-success">Buat Akun</a>
+            <!-- /.card-header -->
+            <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap table-sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>NIP/Nama/Jabatan</th>
+                            <th>Pangkat</th>
+                            <th>Tgl Lahir</th>
+                            <th>Lokasi Presensi</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    @php
+                    $no =1;
+                    @endphp
+                    <tbody>
+                        @foreach ($data as $key => $item)
+                        @if ($item->is_aktif == 0)
+                        <tr style="font-size:11px; font-family:Arial, Helvetica, sans-serif; background-color:#f2dede">
                             @else
-                                <a href="/admin/pegawai/{{$item->id}}/presensi" class="btn btn-xs bg-gradient-warning"><i class="fas fa-calendar"></i> Presensi</a>
-                                <a href="/admin/pegawai/{{$item->id}}/resetpass" class="btn btn-xs bg-gradient-secondary"><i class="fas fa-key"></i> Reset Pass</a>
+                        <tr style="font-size:11px; font-family:Arial, Helvetica, sans-serif; ">
                             @endif
-                        @endif
-                    </td>
-                    </tr>
-                @endforeach
-            </tbody>
-            </table>
-        </div>
-        <!-- /.card-body -->
+                            <td>{{$no++}}</td>
+                            <td>{{$item->nama}}<br />{{$item->nip}}<br />{{$item->jabatan}}</td>
+                            <td>{{$item->pangkat}}</td>
+                            <td>{{\Carbon\Carbon::parse($item->tanggal_lahir)->format('d-m-Y')}}</td>
+                            <td>
+
+                                @if ($item->is_aktif == 0)
+                                @else
+                                @if ($item->lokasi == null)
+                                <a href="/admin/pegawai/{{$item->id}}/lokasi" class="btn btn-xs bg-gradient-primary"><i
+                                        class="fas fa-pen"></i></a>
+                                @else
+                                {{$item->lokasi->nama}}
+                                <a href="/admin/pegawai/{{$item->id}}/editlokasi"
+                                    class="btn btn-xs bg-gradient-success"><i class="fas fa-pen"></i></a>
+                                @endif
+                                @endif
+                            </td>
+                            <td>
+                                @if ($item->is_aktif == 0)
+                                <strong>STATUS : PENSIUN</strong>
+
+                                @else
+                                @if ($item->user == null)
+                                <a href="/admin/pegawai/{{$item->id}}/createuser"
+                                    class="btn btn-xs bg-gradient-success">Buat Akun</a>
+                                @else
+                                <a href="/admin/pegawai/{{$item->id}}/presensi"
+                                    class="btn btn-xs bg-gradient-warning"><i class="fas fa-calendar"></i> Presensi</a>
+                                <a href="/admin/pegawai/{{$item->id}}/resetpass"
+                                    class="btn btn-xs bg-gradient-secondary"><i class="fas fa-key"></i> Reset Pass</a>
+                                @endif
+                                @endif
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
         </div>
         {{$data->links()}}
     </div>
