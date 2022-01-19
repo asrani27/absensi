fd0af25f3a2e4cf02e79e4fdbc473bb353203cd0
asrani <asrani.27@gmail.com>
2022-01-19 08:10:17 +0800

    f

--- resources/views/pegawai/home.blade.php
+++ resources/views/pegawai/home.blade.php
@@ -142,9 +142,9 @@
 
   <!-- /.col -->
   <div class="col-12 col-sm-6 col-md-3">
-    <a href="/pegawai/presensi/barcode" style="color:black">
+    <a href="#" style="color:black">
       <div class="info-box mb-3">
-        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-qrcode"></i></span>
+        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-percent"></i></span>
 
         <div class="info-box-content">
           <span class="info-box-text">PERSENTASE KEHADIRAN</span>
