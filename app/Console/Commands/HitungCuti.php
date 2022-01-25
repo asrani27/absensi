bf74c7103f567b374a89a1c605bffabddd1a4e3a
asrani <asrani.27@gmail.com>
2022-01-25 12:55:59 +0800

    f

--- app/Console/Commands/HitungCuti.php
+++ app/Console/Commands/HitungCuti.php
@@ -47,30 +47,39 @@ class HitungCuti extends Command
 
         foreach ($data as $item) {
             $period = CarbonPeriod::create($item->tanggal_mulai, $item->tanggal_selesai);
-            foreach ($period as $date) {
-                //simpan cuti tahun di presensi
-                $check = Presensi::where('nip', $item->nip)->where('tanggal', $date->format('Y-m-d'))->first();
-                if ($check == null) {
-                    //save
-                    $p = new Presensi;
-                    $p->nip = $item->nip;
-                    $p->nama = $item->nama;
-                    $p->skpd_id = $item->skpd_id;
-                    $p->tanggal = $date->format('Y-m-d');
-                    $p->jam_masuk = '00:00:00';
-                    $p->jam_pulang = '00:00:00';
-                    $p->terlambat = 0;
-                    $p->lebih_awal = 0;
-                    $p->jenis_keterangan_id = null;
-                    $p->save();
-                } else {
-                    $check->update([
-                        'jam_masuk' => '00:00:00',
-                        'jam_pulang' => '00:00:00',
-                        'terlambat' => 0,
-                        'lebih_awal' => 0,
-                        'jenis_keterangan_id' => null,
-                    ]);
+            foreach ($data as $item) {
+                $period = CarbonPeriod::create($item->tanggal_mulai, $item->tanggal_selesai);
+                foreach ($period as $date) {
+                    if ($date->isWeekend()) {
+                    } else {
+                        if (LiburNasional::where('tanggal', $date->format('Y-m-d'))->first() == null) {
+                            //simpan cuti tahun di presensi
+                            $check = Presensi::where('nip', $item->nip)->where('tanggal', $date->format('Y-m-d'))->first();
+                            if ($check == null) {
+                                //save
+                                $p = new Presensi;
+                                $p->nip = $item->nip;
+                                $p->nama = $item->nama;
+                                $p->skpd_id = $item->skpd_id;
+                                $p->tanggal = $date->format('Y-m-d');
+                                $p->jam_masuk = '00:00:00';
+                                $p->jam_pulang = '00:00:00';
+                                $p->terlambat = 0;
+                                $p->lebih_awal = 0;
+                                $p->jenis_keterangan_id = 7;
+                                $p->save();
+                            } else {
+                                $check->update([
+                                    'jam_masuk' => '00:00:00',
+                                    'jam_pulang' => '00:00:00',
+                                    'terlambat' => 0,
+                                    'lebih_awal' => 0,
+                                    'jenis_keterangan_id' => 7,
+                                ]);
+                            }
+                        } else {
+                        }
+                    }
                 }
             }
         }
