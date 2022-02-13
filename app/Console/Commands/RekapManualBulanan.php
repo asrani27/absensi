b08e9bae2cf1900b20d64c02a298180be5b3ca8e
asrani <asrani.27@gmail.com>
2022-02-14 00:56:04 +0800

    f

--- app/Console/Commands/RekapManualBulanan.php
+++ app/Console/Commands/RekapManualBulanan.php
@@ -44,16 +44,20 @@ class RekapManualBulanan extends Command
     {
         $pegawai = Ringkasan::where('bulan', '01')->where('tahun', '2022')->get();
         $pegawai->map(function ($item) {
-            $checkJenisPresensi = Pegawai::where('nip', $item->nip)->first()->jenis_presensi;
-            //cek dia jenis presensi 5 hari kerja gak?
-            if ($checkJenisPresensi == 1) {
-                $presensi = Presensi::where('nip', $item->nip)->whereMonth('tanggal', '01')->whereYear('tanggal', '2022')->get();
-                $item->datang_lambat = $presensi->sum('terlambat');
-                $item->pulang_cepat = $presensi->sum('lebih_awal');
-                $item->jam_kerja = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / 60, 2);
-                $item->persen_kehadiran = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / $item->jumlah_jam * 100, 2);
-                $item->save();
-            } else {
+            try {
+                $checkJenisPresensi = Pegawai::where('nip', $item->nip)->first()->jenis_presensi;
+                //cek dia jenis presensi 5 hari kerja gak?
+                if ($checkJenisPresensi == 1) {
+                    $presensi = Presensi::where('nip', $item->nip)->whereMonth('tanggal', '01')->whereYear('tanggal', '2022')->get();
+                    $item->datang_lambat = $presensi->sum('terlambat');
+                    $item->pulang_cepat = $presensi->sum('lebih_awal');
+                    $item->jam_kerja = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / 60, 2);
+                    $item->persen_kehadiran = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / $item->jumlah_jam * 100, 2);
+                    $item->save();
+                } else {
+                }
+            } catch (\Exception $e) {
+                dd($item);
             }
         });
 
