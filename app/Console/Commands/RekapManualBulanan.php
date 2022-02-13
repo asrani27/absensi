f9b146b81e8ae530a6d842d5d2901fb39fe1d4a1
asrani <asrani.27@gmail.com>
2022-02-14 00:54:59 +0800

    f

--- app/Console/Commands/RekapManualBulanan.php
+++ app/Console/Commands/RekapManualBulanan.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Carbon\Carbon;
 use App\Models\Komando;
+use App\Models\Pegawai;
 use App\Models\Presensi;
 use App\Models\Ringkasan;
 use Illuminate\Console\Command;
@@ -43,12 +44,17 @@ class RekapManualBulanan extends Command
     {
         $pegawai = Ringkasan::where('bulan', '01')->where('tahun', '2022')->get();
         $pegawai->map(function ($item) {
-            $presensi = Presensi::where('nip', $item->nip)->whereMonth('tanggal', '01')->whereYear('tanggal', '2022')->get();
-            $item->datang_lambat = $presensi->sum('terlambat');
-            $item->pulang_cepat = $presensi->sum('lebih_awal');
-            $item->jam_kerja = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / 60, 2);
-            $item->persen_kehadiran = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / $item->jumlah_jam * 100, 2);
-            $item->save();
+            $checkJenisPresensi = Pegawai::where('nip', $item->nip)->first()->jenis_presensi;
+            //cek dia jenis presensi 5 hari kerja gak?
+            if ($checkJenisPresensi == 1) {
+                $presensi = Presensi::where('nip', $item->nip)->whereMonth('tanggal', '01')->whereYear('tanggal', '2022')->get();
+                $item->datang_lambat = $presensi->sum('terlambat');
+                $item->pulang_cepat = $presensi->sum('lebih_awal');
+                $item->jam_kerja = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / 60, 2);
+                $item->persen_kehadiran = round(($item->jumlah_jam - $item->datang_lambat - $item->pulang_cepat) / $item->jumlah_jam * 100, 2);
+                $item->save();
+            } else {
+            }
         });
 
         $com['nama_command'] = 'update rekap bulanan manual perbaikan data bulan januari 2022';
