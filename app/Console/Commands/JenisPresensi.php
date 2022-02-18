258c468a9450891d9121e8807f1ce8d58742ce24
asrani <asrani.27@gmail.com>
2022-02-19 00:01:51 +0800

    f

---
+++ app/Console/Commands/JenisPresensi.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Pegawai;
+use App\Models\Ringkasan;
+use Illuminate\Console\Command;
+
+class JenisPresensi extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jenispresensi';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $data = Pegawai::get();
+        foreach ($data as $item) {
+            $ringkasan = Ringkasan::where('nip', $item->nip)->get();
+            foreach ($ringkasan as $item2) {
+                $item2->update(['jenis_presensi' => $item->jenis_presensi]);
+            }
+        }
+    }
+}
