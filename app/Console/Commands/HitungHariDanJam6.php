0e5c92a259d8e5a876f0c92282875d1c2a1a5be5
asrani <asrani.27@gmail.com>
2022-02-18 23:38:55 +0800

    f

--- app/Console/Commands/HitungHariDanJam6.php
+++ app/Console/Commands/HitungHariDanJam6.php
@@ -3,28 +3,27 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
-use App\Models\Komando;
 use App\Models\Pegawai;
 use Carbon\CarbonPeriod;
 use App\Models\Ringkasan;
 use App\Models\LiburNasional;
 use Illuminate\Console\Command;
 
-class HitungHariDanJamBulanIni extends Command
+class HitungHariDanJam6 extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'hitungharidanjam';
+    protected $signature = 'hitungharidanjam6';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Generate Total Hari Dan Jam Kerja Bulan Ini';
+    protected $description = 'Command description';
 
     /**
      * Create a new command instance.
@@ -43,16 +42,16 @@ class HitungHariDanJamBulanIni extends Command
      */
     public function handle()
     {
-        $bulan = Carbon::now()->format('m');
-        $tahun = Carbon::now()->format('Y');
+        $bulan = Carbon::now()->subMonth(1)->format('m');
+        $tahun = Carbon::now()->subMonth(1)->format('Y');
         $tanggalmerah = LiburNasional::whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun)->get()->pluck('tanggal')->toArray();
         $weekends = [];
-        $start = Carbon::now()->startOfMonth();
-        $end = Carbon::now()->endOfMonth();
+        $start = Carbon::now()->subMonth(1)->startOfMonth();
+        $end = Carbon::now()->subMonth(1)->endOfMonth();
         $period = CarbonPeriod::create($start, $end);
         $dates = [];
         foreach ($period as $date) {
-            if ($date->isWeekend()) {
+            if ($date->translatedFormat('l') == 'Minggu') {
                 array_push($weekends, $date->format('Y-m-d'));
             }
             $dates[] = $date->format('Y-m-d');
@@ -62,13 +61,16 @@ class HitungHariDanJamBulanIni extends Command
 
         $jumlah_jam = [];
         foreach ($jumlah_hari_kerja as $item) {
-            $jumlah_jam[] = Carbon::parse($item)->format('l') == 'Friday' ? 210 : 510;
+            if (Carbon::parse($item)->translatedFormat('l') == 'Jumat') {
+                $jumlah_jam[] = 210;
+            } elseif (Carbon::parse($item)->translatedFormat('l') == 'Sabtu') {
+                $jumlah_jam[] = 420;
+            } else {
+                $jumlah_jam[] = 360;
+            }
         }
-        //dd(array_sum($jumlah_jam));
-        //Update Jumlah Hari Kerja Di Tabel Ringkasan        
-        $ringkasan = Ringkasan::where('bulan', $bulan)->where('tahun', $tahun)->get();
-        //dd($ringkasan);
-        $pegawai = Pegawai::where('jenis_presensi', '1')->get();
+
+        $pegawai = Pegawai::where('jenis_presensi', '2')->get();
         foreach ($pegawai as $item) {
             $check = Ringkasan::where('nip', $item->nip)->where('bulan', $bulan)->where('tahun', $tahun)->first();
             if ($check == null) {
@@ -76,6 +78,7 @@ class HitungHariDanJamBulanIni extends Command
                 $n->nip = $item->nip;
                 $n->nama = $item->nama;
                 $n->jabatan = $item->jabatan;
+                $n->puskesmas_id = $item->puskesmas_id;
                 $n->jumlah_hari = count($jumlah_hari_kerja);
                 $n->jumlah_jam = array_sum($jumlah_jam);
                 $n->save();
@@ -83,13 +86,9 @@ class HitungHariDanJamBulanIni extends Command
                 $item->update([
                     'jumlah_hari' => count($jumlah_hari_kerja),
                     'jumlah_jam' => array_sum($jumlah_jam),
+                    'puskesmas_id' => $item->puskesmas_id,
                 ]);
             }
         }
-        
-        $com['nama_command'] = 'hitung hari dan jam bulan ini';
-        $com['waktu_eksekusi'] = Carbon::now()->format('Y-m-d H:i:s');
-
-        Komando::create($com);
     }
 }
