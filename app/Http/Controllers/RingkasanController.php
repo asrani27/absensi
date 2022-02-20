04c2e6a090c37fa1b3c72aee1d2bcd219de1b7e8
asrani <asrani.27@gmail.com>
2022-02-20 21:26:23 +0800

    f

--- app/Http/Controllers/RingkasanController.php
+++ app/Http/Controllers/RingkasanController.php
@@ -221,7 +221,7 @@ class RingkasanController extends Controller
         $ringkasan = Ringkasan::where('skpd_id', Auth::user()->skpd->id)->where('puskesmas_id', null)->where('bulan', $bulan)->where('tahun', $tahun)->get();
         foreach ($ringkasan as $item) {
 
-            $hadirdiharikerja = count(Presensi::where('nip', $item->nip)->whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun)->where('jam_masuk', '!=', '00:00:00')->orWhere('jam_pulang', '!=', '00:00:00')->get());
+            $hadirdiharikerja = count(Presensi::where('nip', $item->nip)->whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun)->where('jam_masuk', '!=', '00:00:00')->get());
 
             $item->update([
                 'kerja' => $hadirdiharikerja,
