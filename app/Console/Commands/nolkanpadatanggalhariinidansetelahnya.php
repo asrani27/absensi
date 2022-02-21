da0991b4127f43a99e0ec1e0466949cfb0f40a81
asrani <asrani.27@gmail.com>
2022-02-22 07:03:39 +0800

    f

---
+++ app/Console/Commands/nolkanpadatanggalhariinidansetelahnya.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Models\Presensi;
+use Illuminate\Console\Command;
+
+class nolkanpadatanggalhariinidansetelahnya extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nolkanpadatanggalhariinidansetelahnya';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $today = Carbon::now()->format('Y-m-d');
+        $data = Presensi::where('tanggal', '>=', $today)->get();
+        dd($data);
+        foreach ($data as $item) {
+            $item->update([
+                'terlambat' => 0,
+                'lebih_awal' => 0,
+            ]);
+        }
+    }
+}
