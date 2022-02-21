9b83c441319a095d4184fe785090502f2620509e
asrani <asrani.27@gmail.com>
2022-02-21 09:15:50 +0800

    f

--- app/Http/Controllers/RingkasanController.php
+++ app/Http/Controllers/RingkasanController.php
@@ -48,6 +48,7 @@ class RingkasanController extends Controller
     {
         $skpd_id = Auth::user()->skpd->id;
         $pegawai = Pegawai::where('skpd_id', $skpd_id)->where('puskesmas_id', null)->where('is_aktif', 1)->get();
+        dd($pegawai);
         foreach ($pegawai as $item) {
             $check = Ringkasan::where('nip', $item->nip)->where('bulan', $bulan)->where('tahun', $tahun)->first();
             if ($check == null) {
