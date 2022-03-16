f695e41ccec9187e148507506547f89666914419
asrani <asrani.27@gmail.com>
2022-03-16 15:01:38 +0800

    f

--- app/Console/Commands/HitungTerlambatHariIni.php
+++ app/Console/Commands/HitungTerlambatHariIni.php
@@ -126,47 +126,5 @@ class HitungTerlambatHariIni extends Command
         $com['waktu_eksekusi'] = Carbon::now()->format('Y-m-d H:i:s');
 
         Komando::create($com);
-
-        // if (Carbon::parse($tanggal)->isWeekend() == true) {
-        //     $presensi = Presensi::where('tanggal', $tanggal)->get();
-        //     foreach ($presensi as $item) {
-        //         $item->update([
-        //             'terlambat' => 0,
-        //             'lebih_awal' => 0,
-        //         ]);
-        //     }
-        //     toastr()->success('ini adalah hari weekend');
-        // } else {
-        //     if (LiburNasional::where('tanggal', $tanggal)->first() != null) {
-        //         $presensi = Presensi::where('tanggal', $tanggal)->get();
-        //         foreach ($presensi as $item) {
-        //             $item->update([
-        //                 'terlambat' => 0,
-        //                 'lebih_awal' => 0,
-        //             ]);
-        //         }
-        //         toastr()->success('ini adalah hari libur nasional');
-        //     } else {
-        //         $today = $tanggal;
-        //         //Carbon::today()->format('Y-m-d');
-        //         $hari = Carbon::parse($tanggal)->translatedFormat('l');
-        //         //Carbon::today()->translatedFormat('l');
-        //         $jam = Jam::where('hari', $hari)->first();
-        //         //dd($hari, $jam);
-        //         $presensi = Presensi::where('tanggal', $today)->get();
-        //         foreach ($presensi as $item) {
-        //             if ($item->jenis_keterangan_id == 5 || $item->jenis_keterangan_id == 7 || $item->jenis_keterangan_id == 9) {
-        //                 //Cuti tahunan presensinya di akui
-        //             } else {
-        //                 HitungTerlambat::dispatch($item, $jam);
-        //             }
-        //         }
-
-        //         $com['nama_command'] = 'hitung terlambat tanggal' . $today;
-        //         $com['waktu_eksekusi'] = Carbon::now()->format('Y-m-d H:i:s');
-
-        //         Komando::create($com);
-        //     }
-        // }
     }
 }
