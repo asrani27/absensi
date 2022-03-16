87a0c9967e458ee7d50287369ba6f6f59bdec30f
asrani <asrani.27@gmail.com>
2022-03-17 00:49:41 +0800

    f

--- app/Http/Controllers/RingkasanController.php
+++ app/Http/Controllers/RingkasanController.php
@@ -176,7 +176,7 @@ class RingkasanController extends Controller
             return back();
         }
 
-        $ringkasan = Ringkasan::where('skpd_id', Auth::user()->skpd->id)->where('puskesmas_id', null)->where('bulan', $bulan)->where('tahun', $tahun)->get();
+        $ringkasan = Ringkasan::where('skpd_id', Auth::user()->skpd->id)->where('puskesmas_id', null)->where('sekolah_id', null)->where('bulan', $bulan)->where('tahun', $tahun)->get();
         foreach ($ringkasan as $item) {
             if (Pegawai::where('nip', $item->nip)->first()->jenis_presensi == 1) {
                 $jml_hari   = jumlahHari($bulan, $tahun)['jumlah_hari'];
@@ -257,7 +257,7 @@ class RingkasanController extends Controller
             return back();
         }
 
-        $ringkasan = Ringkasan::where('skpd_id', Auth::user()->skpd->id)->where('puskesmas_id', null)->where('bulan', $bulan)->where('tahun', $tahun)->get();
+        $ringkasan = Ringkasan::where('skpd_id', Auth::user()->skpd->id)->where('puskesmas_id', null)->where('sekolah_id', null)->where('bulan', $bulan)->where('tahun', $tahun)->get();
 
         foreach ($ringkasan as $item) {
 
