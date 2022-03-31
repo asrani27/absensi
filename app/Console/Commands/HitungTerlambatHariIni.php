28f84ea58fe2b2f7e0690cc829dc1d10fd37d745
asrani <asrani.27@gmail.com>
2022-03-31 21:05:10 +0800

    f

--- app/Console/Commands/HitungTerlambatHariIni.php
+++ app/Console/Commands/HitungTerlambatHariIni.php
@@ -8,6 +8,7 @@ use App\Models\Jam6;
 use App\Models\Komando;
 use App\Models\Pegawai;
 use App\Models\Presensi;
+use Carbon\CarbonPeriod;
 use App\Models\ErrorData;
 use App\Jobs\HitungTerlambat;
 use App\Models\LiburNasional;
@@ -52,90 +53,96 @@ class HitungTerlambatHariIni extends Command
             $tanggal = Carbon::now()->format('Y-m-d');
         }
 
-        $data = Presensi::where('tanggal', $tanggal)->get();
+        $period = CarbonPeriod::create('2022-01-10', '2022-03-31');
 
-        foreach ($data as $item) {
-            if (Pegawai::where('nip', $item->nip)->first() == null) {
-                $er = new ErrorData;
-                $er->nip = $item->nip;
-                $er->keterangan = 'menghitung terlambat tanggal ' . $tanggal;
-                $er->save();
-            } else {
-                $checkJenisPresensi = Pegawai::where('nip', $item->nip)->first()->jenis_presensi;
-            }
-            //cek dia jenis presensi 5 hari kerja gak?
+        foreach ($period as $p) {
+            $tanggal = $p->format('Y-m-d');
 
-            if ($checkJenisPresensi == 1) {
-                //cek dia tanggalnya weekend gak?
-                if (Carbon::parse($item->tanggal)->isWeekend() == true) {
-                    // weekend 
-                    $item->update([
-                        'terlambat' => 0,
-                        'lebih_awal' => 0,
-                    ]);
+            $data = Presensi::where('tanggal', $tanggal)->get();
+
+            foreach ($data as $item) {
+                if (Pegawai::where('nip', $item->nip)->first() == null) {
+                    $er = new ErrorData;
+                    $er->nip = $item->nip;
+                    $er->keterangan = 'menghitung terlambat tanggal ' . $tanggal;
+                    $er->save();
                 } else {
-                    //cek dia tanggalnya Libur nasional gak?
-                    if (LiburNasional::where('tanggal', $item->tanggal)->first() != null) {
+                    $checkJenisPresensi = Pegawai::where('nip', $item->nip)->first()->jenis_presensi;
+                }
+                //cek dia jenis presensi 5 hari kerja gak?
+
+                if ($checkJenisPresensi == 1) {
+                    //cek dia tanggalnya weekend gak?
+                    if (Carbon::parse($item->tanggal)->isWeekend() == true) {
+                        // weekend 
                         $item->update([
                             'terlambat' => 0,
                             'lebih_awal' => 0,
                         ]);
                     } else {
-                        //cek dia TL / Cuti Tahunan gak?
-                        if ($item->jenis_keterangan_id == 7 || $item->jenis_keterangan_id == 5 || $item->jenis_keterangan_id == 9) {
+                        //cek dia tanggalnya Libur nasional gak?
+                        if (LiburNasional::where('tanggal', $item->tanggal)->first() != null) {
                             $item->update([
                                 'terlambat' => 0,
                                 'lebih_awal' => 0,
-                                'jenis_keterangan_id' => $item->jenis_keterangan_id,
                             ]);
                         } else {
-                            $hari = Carbon::parse($item->tanggal)->translatedFormat('l');
-                            $jam = Jam::where('hari', $hari)->first();
-                            HitungTerlambat::dispatch($item, $jam);
+                            //cek dia TL / Cuti Tahunan gak?
+                            if ($item->jenis_keterangan_id == 7 || $item->jenis_keterangan_id == 5 || $item->jenis_keterangan_id == 9) {
+                                $item->update([
+                                    'terlambat' => 0,
+                                    'lebih_awal' => 0,
+                                    'jenis_keterangan_id' => $item->jenis_keterangan_id,
+                                ]);
+                            } else {
+                                $hari = Carbon::parse($item->tanggal)->translatedFormat('l');
+                                $jam = Jam::where('hari', $hari)->first();
+                                HitungTerlambat::dispatch($item, $jam);
+                            }
                         }
                     }
-                }
-            } elseif ($checkJenisPresensi == 2) {
-                //cek dia tanggalnya minggu gak?
-                if (Carbon::parse($item->tanggal)->translatedFormat('l') == 'Minggu') {
-                    // minggu 
-                    $item->update([
-                        'terlambat' => 0,
-                        'lebih_awal' => 0,
-                    ]);
-                } else {
-                    //cek dia tanggalnya Libur nasional gak?
-                    if (LiburNasional::where('tanggal', $item->tanggal)->first() != null) {
+                } elseif ($checkJenisPresensi == 2) {
+                    //cek dia tanggalnya minggu gak?
+                    if (Carbon::parse($item->tanggal)->translatedFormat('l') == 'Minggu') {
+                        // minggu 
                         $item->update([
                             'terlambat' => 0,
                             'lebih_awal' => 0,
                         ]);
                     } else {
-                        //cek dia TL / Cuti Tahunan gak?
-                        if ($item->jenis_keterangan_id == 7 || $item->jenis_keterangan_id == 5 || $item->jenis_keterangan_id == 9) {
+                        //cek dia tanggalnya Libur nasional gak?
+                        if (LiburNasional::where('tanggal', $item->tanggal)->first() != null) {
                             $item->update([
                                 'terlambat' => 0,
                                 'lebih_awal' => 0,
-                                'jenis_keterangan_id' => $item->jenis_keterangan_id,
                             ]);
                         } else {
-                            $hari = Carbon::parse($item->tanggal)->translatedFormat('l');
-                            $jam = Jam6::where('hari', $hari)->first();
-                            HitungTerlambat::dispatch($item, $jam);
+                            //cek dia TL / Cuti Tahunan gak?
+                            if ($item->jenis_keterangan_id == 7 || $item->jenis_keterangan_id == 5 || $item->jenis_keterangan_id == 9) {
+                                $item->update([
+                                    'terlambat' => 0,
+                                    'lebih_awal' => 0,
+                                    'jenis_keterangan_id' => $item->jenis_keterangan_id,
+                                ]);
+                            } else {
+                                $hari = Carbon::parse($item->tanggal)->translatedFormat('l');
+                                $jam = Jam6::where('hari', $hari)->first();
+                                HitungTerlambat::dispatch($item, $jam);
+                            }
                         }
                     }
+                } else {
+                    $item->update([
+                        'terlambat' => 0,
+                        'lebih_awal' => 0,
+                    ]);
                 }
-            } else {
-                $item->update([
-                    'terlambat' => 0,
-                    'lebih_awal' => 0,
-                ]);
             }
+
+            $com['nama_command'] = 'hitung terlambat tanggal' . $tanggal;
+            $com['waktu_eksekusi'] = Carbon::now()->format('Y-m-d H:i:s');
+
+            Komando::create($com);
         }
-
-        $com['nama_command'] = 'hitung terlambat tanggal' . $tanggal;
-        $com['waktu_eksekusi'] = Carbon::now()->format('Y-m-d H:i:s');
-
-        Komando::create($com);
     }
 }
