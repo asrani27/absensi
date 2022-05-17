cdeff356bc886de28d2051ad72cefd4823bd84e9
asrani <asrani.27@gmail.com>
2022-05-17 08:49:14 +0800

    f

--- resources/views/admin/laporan/tanggal.blade.php
+++ resources/views/admin/laporan/tanggal.blade.php
@@ -71,8 +71,10 @@
 				<td class="auto-style4">{{$item->nama}} <br /> {{$item->nip}}</td>
 				<td class="auto-style3">{{$item->pangkat}}<br />({{$item->golongan}})</td>
 				<td class="auto-style3">{{$item->jabatan}}</td>
-				<td class="auto-style3">{{$item->presensi->jam_masuk}}</td>
-				<td class="auto-style3">{{$item->presensi->jam_pulang}}</td>
+				<td class="auto-style3">{{$item->presensi->jam_masuk == null ? '' :
+					\Carbon\Carbon::parse($item->presensi->jam_masuk)->format('H:i:s')}}</td>
+				<td class="auto-style3">{{$item->presensi->jam_pulang == null ? '' :
+					\Carbon\Carbon::parse($item->presensi->jam_pulang)->format('H:i:s')}}</td>
 			</tr>
 		</tbody>
 		@endforeach
