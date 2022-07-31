fa5b951f46c8f0bbd29ff3708af2eded6e3345e0
asrani <asrani.27@gmail.com>
2022-08-01 06:01:35 +0800

    f

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -80,6 +80,11 @@ class PresensiController extends Controller
         // $response = $client->request('get', Auth::user()->username);
         // $data =  json_decode((string) $response->getBody())->data;
         // $skpd = Skpd::find($data->skpd_id);
+        if (Auth::user()->android_id != null) {
+            toastr()->error('Silahkan Gunakan Versi Android');
+            return back();
+        }
+
         $skpd = Skpd::find(Auth::user()->pegawai->skpd_id);
         if (Auth::user()->pegawai->lokasi == null) {
             $latlong2 = null;
