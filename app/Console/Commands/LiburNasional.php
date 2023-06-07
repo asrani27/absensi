2b702f4d998e03463a33bcc6cc1b4c1a4416d3bb
asrani <asrani.27@gmail.com>
2023-06-07 12:38:02 +0800

    f

---
+++ app/Console/Commands/LiburNasional.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Presensi;
+use Illuminate\Console\Command;
+
+class LiburNasional extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'liburnasional';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $data = LiburNasional::get();
+        foreach ($data as $item) {
+            $check = Presensi::where('tanggal', $item->tanggal)->get();
+            foreach ($check as $item2) {
+                $item->update([
+                    'jam_masuk' => $item->tanggal . ' 00:00:00',
+                    'jam_pulang' => $item->tanggal . ' 00:00:00',
+                    'terlambat' => 0,
+                    'lebih_awal' => 0,
+                    'jenis_keterangan_id' => null,
+                ]);
+            }
+        }
+    }
+}
---
+++ app/Console/Commands/PerbaikanLiburNasional.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Presensi;
+use App\Models\LiburNasional;
+use Illuminate\Console\Command;
+
+class PerbaikanLiburNasional extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'perbaikanliburnasional';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $data = LiburNasional::get();
+        foreach ($data as $item) {
+            $check = Presensi::where('tanggal', $item->tanggal)->get();
+            foreach ($check as $item2) {
+                $item->update([
+                    'jam_masuk' => $item->tanggal . ' 00:00:00',
+                    'jam_pulang' => $item->tanggal . ' 00:00:00',
+                    'terlambat' => 0,
+                    'lebih_awal' => 0,
+                    'jenis_keterangan_id' => null,
+                ]);
+            }
+        }
+    }
+}
