feb8eaa2533c744d579998781a1c3a01b4814acc
asrani <asrani.27@gmail.com>
2023-06-26 10:16:55 +0800

    f

---
+++ app/Models/DetailCutiTpp.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailCutiTpp extends Model
+{
+    use HasFactory;
+
+    protected $table = 'detail_cuti_tpp';
+    protected $guarded = ['id'];
+
+    public function jenis_keterangan()
+    {
+        return $this->belongsTo(JenisKeterangan::class, 'jenis_keterangan_id');
+    }
+    public function skpd()
+    {
+        return $this->belongsTo(Skpd::class, 'skpd_id');
+    }
+}
