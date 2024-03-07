fffb70b9402bbd5640fbc4f70c8604e9e520a500
asrani <asrani.27@gmail.com>
2024-03-07 14:56:12 +0800

    f

--- app/Console/Commands/PerbaikanJamKerja.php
+++ app/Console/Commands/PerbaikanJamKerja.php
@@ -55,33 +55,34 @@ class PerbaikanJamKerja extends Command
             $presensi = Presensi::where('nip', $p->nip)->whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun)->get();
             //dd($presensi, 'asd');
             foreach ($presensi as $pre) {
-
-                if ($pre->jenis_keterangan_id != null) {
-                    return 'cuti';
+                if ($pre->terlambat > 0) {
+                    $pre->update([
+                        'terlambat' => 0,
+                        'jam_masuk' => $pre->tanggal . ' 07:23:09',
+                    ]);
                 } else {
-                    if (Carbon::parse($pre->tanggal)->isWeekend()) {
-                        return 'weekend';
-                    } else {
-                        if (LiburNasional::where('tanggal', $pre->tanggal)->first() != null) {
-                            return 'libur nasional';
-                        } else {
-                            if ($pre->terlambat > 0) {
-                                $pre->update([
-                                    'terlambat' => 0,
-                                    'jam_masuk' => $pre->tanggal . ' 07:23:09',
-                                ]);
-                            } else {
-                            }
-                            if ($pre->lebih_awal > 0) {
-                                $pre->update([
-                                    'jam_pulang' => $pre->tanggal . ' 17:23:09',
-                                    'lebih_awal' => 0,
-                                ]);
-                            } else {
-                            }
-                        }
-                    }
                 }
+                if ($pre->lebih_awal > 0) {
+                    $pre->update([
+                        'jam_pulang' => $pre->tanggal . ' 17:23:09',
+                        'lebih_awal' => 0,
+                    ]);
+                } else {
+                }
+
+                // if ($pre->jenis_keterangan_id != null) {
+                //     return 'cuti';
+                // } else {
+                //     if (Carbon::parse($pre->tanggal)->isWeekend()) {
+                //         return 'weekend';
+                //     } else {
+                //         if (LiburNasional::where('tanggal', $pre->tanggal)->first() != null) {
+                //             return 'libur nasional';
+                //         } else {
+
+                //         }
+                //     }
+                // }
             }
         }
 
