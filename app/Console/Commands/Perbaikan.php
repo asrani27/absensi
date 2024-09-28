6dba8885a47032d9d2b03891b08026a7b0b4ef42
asrani <asrani.27@gmail.com>
2024-09-28 20:01:09 +0800

    f

--- app/Console/Commands/Perbaikan.php
+++ app/Console/Commands/Perbaikan.php
@@ -40,15 +40,18 @@ class Perbaikan extends Command
     public function handle()
     {
 
-        $presensi = Presensi::where('tanggal', '2024-06-25')->get();
+        $presensi = Presensi::where('tanggal', '2024-09-27')->get();
 
         foreach ($presensi as $key => $item) {
             if ($item->jenis_keterangan_id == null) {
                 // if ($item->terlambat != 0) {
-                $item->update([
-                    'jam_pulang' => "2024-06-25 17:10:01",
-                    'lebih_awal' => 0,
-                ]);
+                if ($item->jam_pulang == '2024-09-27 00:00:00') {
+                    $item->update([
+                        'jam_pulang' => "2024-09-27 12:10:01",
+                        'lebih_awal' => 0,
+                    ]);
+                } else {
+                }
                 // } else {
                 // }
             } else {
