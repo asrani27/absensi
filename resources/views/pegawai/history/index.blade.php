b1c6d1a51e1600fb59b73372b316f73e87ae5f74
asrani <asrani.27@gmail.com>
2025-01-08 10:58:05 +0800

    f

--- resources/views/pegawai/history/index.blade.php
+++ resources/views/pegawai/history/index.blade.php
@@ -41,7 +41,7 @@
                                 <option value="2022" {{old('tahun')=='2022' ? 'selected' :''}}>2022</option>
                                 <option value="2023" {{old('tahun')=='2023' ? 'selected' :''}}>2023</option>
                                 <option value="2024" {{old('tahun')=='2024' ? 'selected' :''}}>2024</option>
-                                <option value="2024" {{old('tahun')=='2025' ? 'selected' :''}}>2025</option>
+                                <option value="2025" {{old('tahun')=='2025' ? 'selected' :''}}>2025</option>
                             </select>
                         </div>
                     </div>
