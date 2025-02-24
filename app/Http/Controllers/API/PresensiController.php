1a9b6a5a600e3b4645651137796fa6aa42b67111
asrani <asrani.27@gmail.com>
2025-02-24 08:32:25 +0800

    f

--- app/Http/Controllers/API/PresensiController.php
+++ app/Http/Controllers/API/PresensiController.php
@@ -90,7 +90,7 @@ class PresensiController extends Controller
 
         // Batasan waktu absensi
         $startTime = '06:00';
-        $startTime2 = '08:45';
+        $startTime2 = '08:30';
         $endTime = '20:00';
         $endTime2 = '09:15';
 
