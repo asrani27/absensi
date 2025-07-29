f5ad38a0414cba3f5b82ce5c523c34b6885194a1
asrani <asrani.27@gmail.com>
2025-07-29 16:27:48 +0800

    f

--- app/Exports/laporan2022.php
+++ app/Exports/laporan2022.php
@@ -18,7 +18,7 @@ class laporan2022 implements FromView, WithEvents
     public function view(): View
     {
         // Ambil semua pegawai yang aktif
-        $pegawai = Pegawai::where('status_asn', null)->get();
+        $pegawai = Pegawai::where('status_asn', null)->where('is_aktif', 1)->get();
 
         // Ambil data ringkasan yang relevan sekaligus untuk semua pegawai pada tahun 2023 dan seluruh bulan (Januari - Desember)
         $ringkasan = Ringkasan::whereIn('nip', $pegawai->pluck('nip'))
