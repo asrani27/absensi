e54ccb61373c2fa7b67d6968fd9de29711e72a60
asrani <asrani.27@gmail.com>
2025-08-17 08:08:56 +0800

    f

--- app/Http/Controllers/API/PresensiController.php
+++ app/Http/Controllers/API/PresensiController.php
@@ -229,7 +229,7 @@ class PresensiController extends Controller
                     $param['jam_masuk'] = Carbon::now()->format('Y-m-d H:i:s');
                     Presensi::create($param);
                     $data['message_error'] = 200;
-                    $data['message']       = 'Berhasil Di Simpan, presensi hari besar tidak di tampilkan di beranda anda';
+                    $data['message']       = 'Berhasil Di Simpan';
                 } else {
                     if ($check->jam_masuk_hari_besar == null || Carbon::parse($check->jam_masuk_hari_besar)->format('H:i:s') == '00:00:00') {
                         $check->update($param);
@@ -237,10 +237,10 @@ class PresensiController extends Controller
                             $check->update(['jam_masuk' => Carbon::now()->format('Y-m-d H:i:s')]);
                         }
                         $data['message_error'] = 200;
-                        $data['message']       = 'Presensi Masuk Berhasil Di Update, presensi hari besar tidak di tampilkan di beranda anda';
+                        $data['message']       = 'Presensi Masuk Berhasil Di Update';
                     } else {
                         $data['message_error'] = 200;
-                        $data['message']       = 'Anda Sudah Absen, presensi hari besar tidak di tampilkan di beranda anda';
+                        $data['message']       = 'Anda Sudah Absen';
                     }
                 }
 
