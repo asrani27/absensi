76c1ee20ba4c8d44c69dd846af95062ded061bf4
asrani <asrani.27@gmail.com>
2025-10-14 10:15:01 +0800

    f

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,29 +21,35 @@ class LoginController extends Controller
     public function login(Request $req)
     {
 
-        if (Auth::attempt(['username' => $req->username, 'password' => $req->password], true)) {
-
-            if (Auth::user()->hasRole('pegawai')) {
-                if (Auth::user()->pegawai->is_aktif == 0) {
-                    Auth::logout();
-                    toastr()->error('Anda Telah NonAktif');
-                    return redirect('/');
-                } else {
-                    return redirect('/home/pegawai');
-                }
-            } elseif (Auth::user()->hasRole('admin')) {
-                return redirect('/home/admin');
-            } elseif (Auth::user()->hasRole('superadmin')) {
-                return redirect('/home/superadmin');
-            } elseif (Auth::user()->hasRole('puskesmas')) {
-                return redirect('/home/puskesmas');
-            } elseif (Auth::user()->hasRole('mod')) {
-                return redirect('/home/mod');
-            }
-        } else {
-            toastr()->error('Username / Password Tidak Ditemukan');
+        if ($req->username == '199904262024211002') {
+            toastr()->error('Please Install From AppStore / PlayStore');
             $req->flash();
             return back();
+        } else {
+            if (Auth::attempt(['username' => $req->username, 'password' => $req->password], true)) {
+
+                if (Auth::user()->hasRole('pegawai')) {
+                    if (Auth::user()->pegawai->is_aktif == 0) {
+                        Auth::logout();
+                        toastr()->error('Anda Telah NonAktif');
+                        return redirect('/');
+                    } else {
+                        return redirect('/home/pegawai');
+                    }
+                } elseif (Auth::user()->hasRole('admin')) {
+                    return redirect('/home/admin');
+                } elseif (Auth::user()->hasRole('superadmin')) {
+                    return redirect('/home/superadmin');
+                } elseif (Auth::user()->hasRole('puskesmas')) {
+                    return redirect('/home/puskesmas');
+                } elseif (Auth::user()->hasRole('mod')) {
+                    return redirect('/home/mod');
+                }
+            } else {
+                toastr()->error('Username / Password Tidak Ditemukan');
+                $req->flash();
+                return back();
+            }
         }
     }
 }
